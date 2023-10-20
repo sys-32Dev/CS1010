c819793aaa112a451a8884090f4ea3f654f17238
sys_32 <70983906+sys-32Dev@users.noreply.github.com>
2023-10-20 17:18:24 +0000

    Lab 9

---
+++ Lab 9/Lab 9-1.py
@@ -0,0 +1,33 @@
+# Edwin Hui
+red = "A Red Fox "
+blue = "Chased a Blue Mouse "
+green = "Across a Green Meadow."
+print(red + blue + green)
+# Defining the varibles red, blue and greenm and printing them out.
+print("----------------")
+complete = red + blue + green
+print(complete)
+# defining the varible complete as the combination of red, blue and green and printing it out.
+print("----------------")
+print(complete.lower())
+print(complete)
+# prints out var complete in all lowercasem then prints it out the original to prove that the varible has not been modified.
+print("----------------")
+print(complete.upper())
+print(complete)
+print("----------------")
+print(complete[19])
+# prints index 19 in var complete.
+print("----------------")
+print(complete[19:23])
+# prints index 19-23 in var complete.
+print("----------------")
+print(len(complete))
+# prints the count of characters in var complete.
+print("----------------")
+print(complete.isnumeric())
+# checks if var complete can be converted to numeric and prints true or false
+print("----------------")
+convvar = "1938"
+print(convvar.isnumeric())
+# define convar as a numeric string and print true or false if it can be converted to numeric.
